0ef026b7500536f6a8652f5e6a2297eccb4786af
Ingrid Castillo <cursosdecomputacion.net@gmail.com>
2022-08-03 16:52:56 -0600

    Agregando programa Pilares-POO

---
+++ Ejercicios/pilaresPoo/Animal.cs
@@ -0,0 +1,53 @@
+using System;
+
+namespace pilaresPoo
+{
+   public class Animal
+   {
+   protected string Nombre;
+
+   public string _Nombre { get => Nombre; set => Nombre = value; }
+
+   public void comer()
+   {
+   Console.WriteLine(_Nombre+ " esta comiendo");
+   }
+   public void dormir()
+   {
+   Console.WriteLine(_Nombre+ " esta durmiendo");
+   }
+
+  public void nadar()
+  {
+   Console.WriteLine(_Nombre+ " esta nadando");
+  }
+
+  public void volarAlto()
+  {
+   Console.WriteLine(_Nombre+ " esta volando");
+  }
+  public void ladrar()
+  {
+   Console.WriteLine(_Nombre+ " esta ladrando");
+  }
+
+  public void maullar()
+  {
+   Console.WriteLine(_Nombre+ " esta maullando");
+  }
+  public void hablar()
+  {
+   Console.WriteLine(_Nombre+ " esta hablando");
+  }
+  public void inflarse()
+  {
+   Console.WriteLine(_Nombre+ " esta inflandose");
+   }
+   public Animal(string nombre)
+   {
+    Nombre=nombre;
+   }
+
+   }
+   
+}
---
+++ Ejercicios/pilaresPoo/Peces.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace pilaresPoo
+{
+    public class Peces : Animal
+    {
+        public Peces(string nombre) : base(nombre)
+        {
+            Nombre=nombre;
+        }
+    }
+}
